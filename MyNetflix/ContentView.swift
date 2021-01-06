557a567bf7fcecac300c8d6f22a6aeedaafa66b8
Kicker Chen <darkvrsaint@gmail.com>
2021-01-06 15:43:14 +0800

    create the project

---
+++ MyNetflix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyNetflix
+//
+//  Created by CHENCHIAN on 2021/1/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyNetflix/MyNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNetflixApp.swift
+//  MyNetflix
+//
+//  Created by CHENCHIAN on 2021/1/6.
+//
+
+import SwiftUI
+
+@main
+struct MyNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
